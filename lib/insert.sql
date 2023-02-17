629e85ca0cbd2dfbce1a92d77d8709d72860989c
levy <lnjiru43@gmail.com>
2023-02-17 09:09:57 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex) VALUES ("Mr. Chocolate",8, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Rowdy", 8, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Tabitha", 8, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Sergeant Brown", 8, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Melissa", 8, "F");
+INSERT INTO bears (name, age, sex) VALUES ("Grinch", 8, "M");
+INSERT INTO bears (name, age, sex) VALUES ("Wendy", 8, "F");
+INSERT INTO bears (name, age, sex) VALUES (NULL, NULL, NULL);
